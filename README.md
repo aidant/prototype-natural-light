30b9e9a4b7d64f841c52297e3fff127f0284fb5b
aidant <15520814+aidant@users.noreply.github.com>
2023-08-20 12:47:47 +1000

    wip reading serial

--- README.md
+++ README.md
@@ -4,5 +4,6 @@ rustup target add thumbv7em-none-eabihf
 
 # https://probe.rs/docs/getting-started/installation/
 cargo install probe-rs --features cli,ftdi
+cargo install probe-rs-cli
 cargo flash --release --chip STM32F411CEUx
 ```
--- src/main.rs
+++ src/main.rs
@@ -2,26 +2,51 @@
 #![no_main]
 #![no_std]
 
-extern crate panic_halt;
+extern crate panic_rtt_target;
 
 use cortex_m_rt::entry;
-use stm32f4xx_hal::{pac, prelude::*};
+use rtt_target::{rprintln, rtt_init_print};
+use stm32f4xx_hal::{
+    pac::{self},
+    prelude::*,
+    serial::Config,
+};
 
 #[entry]
 fn main() -> ! {
-    let dp = pac::Peripherals::take().unwrap();
-    let cp = cortex_m::peripheral::Peripherals::take().unwrap();
+    rtt_init_print!();
 
-    let gpioc = dp.GPIOC.split();
-    let mut led = gpioc.pc13.into_push_pull_output();
+    let dp = pac::Peripherals::take().unwrap();
+    // let cp = cortex_m::peripheral::Peripherals::take().unwrap();
 
     let rcc = dp.RCC.constrain();
     let clocks = rcc.cfgr.sysclk(48.MHz()).freeze();
 
-    let mut delay = cp.SYST.delay(&clocks);
+    // let mut delay = cp.SYST.delay(&clocks);
+
+    let gpiob = dp.GPIOB.split();
+    let tx1 = gpiob.pb6.into_alternate();
+    let rx1 = gpiob.pb7.into_alternate();
+
+    let mut serial = dp
+        .USART1
+        .serial::<u8>(
+            (tx1, rx1),
+            Config::default().baudrate(9600_u32.bps()),
+            &clocks,
+        )
+        .unwrap();
+
+    // let gpioc = dp.GPIOC.split();
+    // let mut led = gpioc.pc13.into_push_pull_output();
 
     loop {
-        led.toggle();
-        delay.delay_ms(500_u32);
+        // led.toggle();
+        // rprintln!("Hello World!");
+        // delay.delay_ms(2000_u32);
+
+        rprintln!("Hello World!");
+        let byte = nb::block!(serial.read()).unwrap();
+        rprintln!("here is the byte: {}", byte);
     }
 }
