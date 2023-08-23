b7906ab449319187d513c69920f6c246d7d5f0d4
aidant <15520814+aidant@users.noreply.github.com>
2023-08-23 15:15:19 +1000

    embassy hello world

--- README.md
+++ README.md
@@ -1,8 +1,9 @@
 ```
 rustup update
-rustup target add thumbv7em-none-eabihf
+rustup install nightly
+rustup target add thumbv7em-none-eabi
 
 # https://probe.rs/docs/getting-started/installation/
 cargo install probe-rs --features cli,ftdi
-cargo flash --release --chip STM32F411CEUx
+cargo +nightly run
 ```
--- src/main.rs
+++ src/main.rs
@@ -1,95 +1,29 @@
 #![deny(unsafe_code)]
 #![no_main]
 #![no_std]
+#![feature(type_alias_impl_trait)]
 
-extern crate panic_rtt_target;
+extern crate defmt_rtt;
+extern crate panic_probe;
 
-use cortex_m_rt::entry;
-use heapless::Vec;
-use nmea::Nmea;
-use rtt_target::{rprint, rprintln, rtt_init_print};
-use stm32f4xx_hal::{
-    pac::{self},
-    prelude::*,
-    serial::Config,
-};
-// mod light_characteristics;
+use defmt::*;
+use embassy_executor::Spawner;
+use embassy_stm32::gpio::{Level, Output, Speed};
+use embassy_time::{Duration, Timer};
 
-use chrono::{TimeZone, Utc as UTC};
-// use light_characteristics::get_light_characteristics;
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_stm32::init(Default::default());
 
-// fn print_light_characteristics() {
-//     let datetime = UTC.with_ymd_and_hms(2023, 8, 20, 3, 30, 0).unwrap();
+    info!("Hello World!");
 
-//     rprintln!("{:?}", datetime);
-
-//     let position = light_characteristics::Coordinates {
-//         lat: -33.8688,
-//         lon: 151.2093,
-//     };
-
-//     let light_characteristics = get_light_characteristics(datetime, position).unwrap();
-
-//     rprintln!("{:?}", light_characteristics);
-// }
-
-#[entry]
-fn main() -> ! {
-    rtt_init_print!();
-
-    let mut nmea = Nmea::default();
-
-    // print_light_characteristics();
-
-    let dp = pac::Peripherals::take().unwrap();
-    // let cp = cortex_m::peripheral::Peripherals::take().unwrap();
-
-    let rcc = dp.RCC.constrain();
-    let clocks = rcc.cfgr.sysclk(48.MHz()).freeze();
-
-    // let mut delay = cp.SYST.delay(&clocks);
-
-    let gpiob = dp.GPIOB.split();
-    let tx1 = gpiob.pb6.into_alternate();
-    let rx1 = gpiob.pb7.into_alternate();
-
-    let mut serial = dp
-        .USART1
-        .serial::<u8>(
-            (tx1, rx1),
-            Config::default().baudrate(9600_u32.bps()),
-            &clocks,
-        )
-        .unwrap();
-
-    // let gpioc = dp.GPIOC.split();
-    // let mut led = gpioc.pc13.into_push_pull_output();
-
-    let mut message = Vec::<u8, 82>::new();
+    let mut led = Output::new(p.PC13, Level::High, Speed::Low);
 
     loop {
-        // led.toggle();
-        // rprintln!("Hello World!");
-        // delay.delay_ms(2000_u32);
+        led.set_high();
+        Timer::after(Duration::from_millis(300)).await;
 
-        // rprintln!("Hello World!");
-
-        match nb::block!(serial.read()) {
-            Ok(byte) => {
-                message.push(byte).unwrap();
-
-                if byte == b'\n' {
-                    let sentence = core::str::from_utf8(&message).unwrap();
-                    rprint!("{}", sentence);
-
-                    nmea.parse(sentence).unwrap();
-                    rprintln!("{}", nmea);
-
-                    message.clear();
-                }
-            }
-            Err(error) => rprintln!("unknown error {}", error),
-        }
-        // ;
+        led.set_low();
+        Timer::after(Duration::from_millis(300)).await;
     }
 }
