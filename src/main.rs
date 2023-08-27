ecbb695676c62deca2910d940962a94c8a78e4dc
aidant <15520814+aidant@users.noreply.github.com>
2023-08-27 15:59:22 +1000

    get datetime and coordinates upfront

--- src/main.rs
+++ src/main.rs
@@ -6,8 +6,11 @@
 extern crate defmt_rtt;
 extern crate panic_probe;
 
+use chrono::{DateTime, Utc};
 use defmt::*;
 use embassy_executor::Spawner;
+use embassy_stm32::peripherals;
+use feature_gnss::Coordinates;
 
 use crate::{
     device_adafruit_neopixel_ring::AdafruitNeoPixelRing,
@@ -22,19 +25,13 @@ mod feature_gnss;
 mod light_characteristics;
 mod util_lc_to_rgb;
 
-#[embassy_executor::main]
-async fn main(_spawner: Spawner) {
-    let p = embassy_stm32::init(Default::default());
-
-    info!("Hello World!");
-
-    let mut adafruit_ultimate_gps = AdafruitUltimateGps::new(p.USART1, p.PB7, p.DMA2_CH2);
-    let mut adafruit_neopixel_ring =
-        AdafruitNeoPixelRing::new(p.SPI1, p.PA7, p.DMA2_CH3, p.DMA2_CH0);
-
+async fn get_datetime_and_coordinates(
+    peri: peripherals::USART1,
+    rx_pin: peripherals::PB7,
+    rx_dma: peripherals::DMA2_CH2,
+) -> (DateTime<Utc>, Coordinates) {
     let mut gnss = Gnss::new();
-
-    adafruit_neopixel_ring.write_off().await;
+    let mut adafruit_ultimate_gps = AdafruitUltimateGps::new(peri, rx_pin, rx_dma);
 
     loop {
         let message = match adafruit_ultimate_gps.read_message().await {
@@ -45,19 +42,31 @@ async fn main(_spawner: Spawner) {
             }
         };
 
-        // info!("{}", message);
-
         if let (Some(datetime), Some(coordinates)) = gnss.parse_message(&message) {
-            let lc = get_light_characteristics(datetime, coordinates).unwrap();
-
-            info!(
-                "brightness: {} color_temperature: {}",
-                lc.brightness, lc.color_temperature
-            );
-
-            adafruit_neopixel_ring
-                .write_light_characteristics(&lc)
-                .await;
+            return (datetime, coordinates);
         }
     }
+}
+
+#[embassy_executor::main]
+async fn main(_spawner: Spawner) {
+    let p = embassy_stm32::init(Default::default());
+
+    let mut adafruit_neopixel_ring =
+        AdafruitNeoPixelRing::new(p.SPI1, p.PA7, p.DMA2_CH3, p.DMA2_CH0);
+
+    adafruit_neopixel_ring.write_off().await;
+
+    let (datetime, coordinates) = get_datetime_and_coordinates(p.USART1, p.PB7, p.DMA2_CH2).await;
+
+    let lc = get_light_characteristics(datetime, coordinates).unwrap();
+
+    info!(
+        "brightness: {} color_temperature: {}",
+        lc.brightness, lc.color_temperature
+    );
+
+    adafruit_neopixel_ring
+        .write_light_characteristics(&lc)
+        .await;
 }
